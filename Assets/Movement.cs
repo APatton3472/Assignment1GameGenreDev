8885219cb94ec5484c4e551fd4d4b078e43d8772
APatton3472 <60272264+APatton3472@users.noreply.github.com>
2020-01-24 14:37:53 -0500

    1/24/2020

---
+++ Assets/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMov = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        float yMov = Input.GetAxis("Jump") * speed * Time.deltaTime;
+
+        this.transform.position = new Vector3(this.transform.position.x + xMov, this.transform.position.y, this.transform.position.z);
+
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + yMov, this.transform.position.z);
+    }
+}
