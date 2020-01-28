6ad06a1079911af80e235046fa225f4426d63c45
APatton3472 <60272264+APatton3472@users.noreply.github.com>
2020-01-27 19:16:11 -0500

    1/27/2020

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -7,19 +7,18 @@ public class EnemyMovement : MonoBehaviour
     //found at https://forum.unity.com/threads/moving-an-object-back-and-forth-on-a-single-axis-automatically.235033/
     public float min = 2f;
     public float max = 3f;
+    
     // Use this for initialization
     void Start()
     {
-
-        min = transform.position.x;
-        max = transform.position.x + 3;
-
+        min = transform.position.x-min;
+        max = transform.position.x+max;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
 
         transform.position = new Vector3(Mathf.PingPong(Time.time * 2, max - min) + min, transform.position.y, transform.position.z);
 
---
+++ Assets/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //https://www.youtube.com/watch?v=l6rOHJitheY
+    //
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
