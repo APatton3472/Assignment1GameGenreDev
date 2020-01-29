7eb950b9a63104d8eb14059d60052652e2b58447
APatton3472 <60272264+APatton3472@users.noreply.github.com>
2020-01-28 22:04:41 -0500

    1/28/2020

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,17 +4,30 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    //https://www.youtube.com/watch?v=l6rOHJitheY
-    //
+    public int currentHealth;
+    public int maxHealth = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth;  
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        if (currentHealth <= 0)
+        {
+            Die();
+        } 
+    }
+
+    void Die()
+    {
+        Application.LoadLevel(Application.loadedLevel);
     }
 }
